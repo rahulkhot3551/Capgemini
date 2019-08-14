b47f3bf452562dc53d0d52ab3a7b8df4169e40fd
rahulkhot3551 <36168347+rahulkhot3551@users.noreply.github.com>
2019-08-14 18:05:51 +0530

    Add files via upload

---
+++ Main2.java
@@ -0,0 +1,41 @@
+package com.demo2;
+
+/**
+ * 
+ * @author spavanku
+ * 
+ * accept string as an input from the user, and return the output as string by replacing 
+ * the each and every character with next character of the given input.
+ * 
+ *  sample Input 1:
+ *  =============
+ *  capgemini
+ *  
+ *  Sample output 1:
+ *  ==============
+ *  dbqfnjoj
+ * 
+ * 	sample Input 2:
+ *  ===============
+ *  apple
+ * 
+ *  Sample output 2:
+ *  ==============
+ *  bqqmf
+ *  
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static String getString(String organization) {
+		
+		return null;
+
+	}
+
+
+}
---
+++ Main3.java
@@ -0,0 +1,75 @@
+package com.demo1;
+
+import java.util.HashMap;
+import java.util.List;
+
+
+/**
+ * 
+ * @author spavanku
+ * 
+ * take the HashMap as a first input which contains employee name as a key and his/her organization as a value. 
+ * and organization name of string type as a second input.
+ * 
+ * store all the employee names in List when the second input is matching with the value of the the HashMap.
+ * 
+ *  sample Input 1:
+ *  =============
+ *  How many employees you want to add?
+ *  4
+ *  add 4 Employees
+ *  pavan
+ *  capgemini
+ *  kumar
+ *  cts
+ *  selvi
+ *  ibm
+ *  ravi
+ *  capgemini
+ *  
+ *  Enter organization name
+ *  capgemini
+ *  
+ *  Sample output 1:
+ *  ==============
+ *  pavan
+ *  ravi
+ * 
+ * Note:
+ * =====
+ *  * if No employees present with the given org name print "No employees present with the given organization name"
+ * 
+ * 	sample Input 2:
+ *  ===============
+ *  How many employees you want to add?
+ *  3
+ *  add 3 Employees
+ *  pavan
+ *  capgemini
+ *  kumar
+ *  cts
+ *  selvi
+ *  ibm
+ *    
+ *  Enter organization name
+ *  tcs
+ *  
+ *  Sample output 2:
+ *  ==============
+ *  No employees present with the given organization name
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static List<String> getEmployeeNames(HashMap<String, String> employeesList,String organization) {
+		
+		return null;
+
+	}
+
+}
