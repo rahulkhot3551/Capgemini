dd2a97e9b200fe2d40202c4819133c556a4dbd87
rahulkhot3551 <36168347+rahulkhot3551@users.noreply.github.com>
2019-08-14 18:02:42 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,38 @@
+package com.demo3;
+
+import java.util.List;
+
+/**
+ * 
+ * @author spavanku
+ *
+ *	take List of Strings as input, and return the string which is having max length as an output.
+ *
+ *	Input1:
+ *	======
+ *	How many names you want to add:
+ *	4
+ *	pavan
+ *	kumar
+ *	siva
+ *	sudheer
+ *
+ * Output1:
+ * ========
+ * sudheer
+ *
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// your logic here
+	}
+
+	public static String getnames(List<String> names) {
+
+		return null;
+	}
+
+}
